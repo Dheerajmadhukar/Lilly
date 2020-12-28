01488a6efc882e51a27fabc16de7f7cc8969a613
me_dheeraj <9442273+Dheerajmadhukar@users.noreply.github.com>
2020-12-29 02:22:40 +0530

    Update lilly.sh

--- lilly.sh
+++ lilly.sh
@@ -23,7 +23,8 @@ cat favicon_targets.txt | xargs -I %% bash -c "curl %% -k -s -o /dev/null -w '%%
 cat favicon_urls.txt | awk '{print $1}'| xargs -I %% bash -c "echo %% ; curl -s -L -k %% | python3 -c 'import mmh3,sys,codecs; print(mmh3.hash(codecs.encode(sys.stdin.buffer.read(),\"base64\")))'"> hash.txt
 cat hash.txt | awk 'NR%2{printf "%s ",$0;next;}1' > favi_hash.txt;rm hash.txt -f
 for hasshh in $(cat favi_hash.txt|awk '{print $2}');do shodan search http.favicon.hash:$hasshh --fields ip_str,port --separator " " | awk '{print $1 ":" $2}' > favicon_ips.txt;done
-for cert in $(cat favicon_ips.txt);do curl --insecure -v $cert 2>&1 | awk 'BEGIN { cert=0 } /^\* SSL connection/ { cert=1 } /^\*/ { if (cert) print }' grep 'https:\/\/\|CN\=\|issuer:' | tee all_certs.txt ;done
+cat favicon_ips.txt | httpx -threads 500 -silent | interlace -threads 100 -c "echo _target_; curl --insecure -v _target_ 2>&1 | awk 'BEGIN { cert=0 } /^\* SSL connection/ { cert=1 } /^\*/ { if (cert) print }'" --silent | grep "https:\/\/\|CN\=\|issuer: " | tee all_certs.txt
+#for cert in $(cat favicon_ips.txt);do curl --insecure -v $cert 2>&1 | awk 'BEGIN { cert=0 } /^\* SSL connection/ { cert=1 } /^\*/ { if (cert) print }' grep 'https:\/\/\|CN\=\|issuer:' | tee all_certs.txt ;done
 }
 target=False
 shodan_api=False
