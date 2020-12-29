e48cfc20cd40fa6f1352c5926e0f178839d7efb4
me_dheeraj <9442273+Dheerajmadhukar@users.noreply.github.com>
2020-12-29 19:12:47 +0530

    Update lilly.sh

--- lilly.sh
+++ lilly.sh
@@ -21,9 +21,20 @@ echo -e ">> \e[36mFavicon Hash\e[0m is in progress"
 cat alive.txt | xargs -I %% bash -c 'echo "%%/favicon.ico"' > favicon_targets.txt
 cat favicon_targets.txt | xargs -I %% bash -c "curl %% -k -s -o /dev/null -w '%% ''%{http_code}\n' -X GET | grep 200" > favicon_urls.txt;rm favicon_targets.txt
 cat favicon_urls.txt | awk '{print $1}'| xargs -I %% bash -c "echo %% ; curl -s -L -k %% | python3 -c 'import mmh3,sys,codecs; print(mmh3.hash(codecs.encode(sys.stdin.buffer.read(),\"base64\")))'"> hash.txt
-cat hash.txt | awk 'NR%2{printf "%s ",$0;next;}1' > favi_hash.txt;rm hash.txt -f
-for hasshh in $(cat favi_hash.txt|awk '{print $2}');do shodan search http.favicon.hash:$hasshh --fields ip_str,port --separator " " | awk '{print $1 ":" $2}' > favicon_ips.txt;done
-cat favicon_ips.txt | httpx -threads 500 -silent | interlace -threads 100 -c "echo _target_; curl --insecure -v _target_ 2>&1 | awk 'BEGIN { cert=0 } /^\* SSL connection/ { cert=1 } /^\*/ { if (cert) print }'" --silent | grep "https:\/\/\|CN\=\|issuer: " | tee all_certs.txt
+cat hash.txt | awk 'NR%2{printf "%s ",$0;next;}1' | awk '{print $2}' | grep -v '^0$' | tee favicon_hash.txt
+if [[ -z "$(cat favicon_hash.txt)" ]];then
+        echo -e ">> \e[36mNo hash found on /favicon.ico\e[0m"
+        exit
+else
+        for hasshh in $(cat favicon_hash.txt);do shodan search http.favicon.hash:$hasshh --fields ip_str,port --separator " " | grep -v 'Error' | awk '{print $1 ":" $2}' > favicon_ips.txt;done
+        echo -e ">> \e[36mLilly $yellow[Alert]$end: shodan API credits used: $(cat favicon_hash.txt | wc -l)"
+fi
+if [[ -z "$(cat favicon_ips.txt)" ]];then
+        echo -e ">> \e[36mNo IPs found \e[0m"
+        exit
+else
+cat favicon_ips.txt | grep -v "^\:" | httpx -threads 500 -silent | interlace -threads 100 -c "echo _target_; curl --insecure -v _target_ 2>&1 | awk 'BEGIN { cert=0 } /^\* SSL connection/ { cert=1 } /^\*/ { if (cert) print }'" --silent | grep "https:\/\/\|CN\=\|issuer: " | tee all_certs.txt
+fi
 }
 target=False
 shodan_api=False
